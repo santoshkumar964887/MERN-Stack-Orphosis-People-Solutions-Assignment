b3d614d5291c1820d592424d190cc5a5d57aaa90
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-12 03:18:05 +0530

    setup files for frontend

---
+++ client-side/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import './App.css';
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+    search:"",
+    error: "",
+    posts: [],
+    };
+
+  }
+
+
+ render() {
+    
+    return (
+      <>
+       santosh
+    </>
+    );
+  }
+}
+
+export default App;
