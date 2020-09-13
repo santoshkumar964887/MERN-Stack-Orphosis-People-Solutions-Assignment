bc7fc22c8e8b31cc30037d844a73b82d2fdb2ee6
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-13 12:08:33 +0530

    adding serch feild

--- client-side/src/components/view/view.jsx
+++ client-side/src/components/view/view.jsx
@@ -1,23 +1,45 @@
-import React,{useEffect,useState}  from 'react'
-import axios from '../axios/axios';
-import './view.style.scss';
-const View=()=>{
-    const [data,setdata]=useState([])
- useEffect(()=>{
-    axios.get('/').then(res=>setdata(res.data.data.user));
-  },[])
+import React, { useEffect, useState } from "react";
+import axios from "../axios/axios";
+import "./view.style.scss";
+const View = () => {
+  const [data, setdata] = useState([]);
+  const [datachange, setdatachange] = useState("");
+  const [serchName, setserchNmae] = useState("");
+  useEffect(() => {
+    axios.get("/").then((res) => setdata(res.data.data.user));
+  }, [datachange]);
 
- const handleDelete=(e)=>{
- console.log(e.target.name)
- }
- 
-   console.log(data); 
-    return (
-      <div className="container1">
-        <table className="table table-hover">
+  const handleDelete = (e) => {
+    const url = `/${e.target.name}`;
+    axios.delete(url).then((res) => console.log(res));
+    setdatachange("true");
+  };
+  const handleEdit = (e) => {
+    const url = `/${e.target.name}`;
+    //axios.patch(url).then(res=> console.log(res));
+  };
+  const handleSerch=(e)=>{
+    e.preventDefault();
+    const data1=data.includes(serchName);
+    console.log(data1)
+  }
+  const handleSerchInput=(e)=>{
+    setserchNmae( e.target.value);
+  }
+  console.log(serchName);
+  console.log(data);
+  return (
+    <div className="container1">
+       <nav className="navbar navbar-light bg-light">
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" value={serchName} onChange={(e)=>handleSerchInput(e)} placeholder="Search By First Name" aria-label="Search"/>
+    <button className="btn btn-outline-dark my-2 my-sm-0" onClick={(e)=>handleSerch(e)} type="submit">Search</button>
+  </form>
+</nav>
+      <table className="table table-hover">
         <thead className="thead-dark">
+         
           <tr>
-           
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
             <th scope="col">Email</th>
@@ -28,22 +50,38 @@ const View=()=>{
           </tr>
         </thead>
         <tbody>
-          {
-            data.map(el=>(   <tr>
+          {data.map((el) => (
+            <tr>
               <td>{el.firstName}</td>
-            <td>{el.LastName}</td>
-            <td>{el.Email}</td>
-            <td>{el.BOB}</td>
-            <td>{el.Short_BIO}</td>
-              <td><button type="button" class="btn btn-outline-primary">Primary</button></td>
-              <td><button type="button" name={el._id} onClick={(e)=>handleDelete(e)} className="btn btn-outline-danger">Delete</button></td>
+              <td>{el.LastName}</td>
+              <td>{el.Email}</td>
+              <td>{el.BOB}</td>
+              <td>{el.Short_BIO}</td>
+              <td>
+                <button
+                  type="button"
+                  name={el._id}
+                  onClick={(e) => handleEdit(e)}
+                  class="btn btn-outline-primary"
+                >
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  name={el._id}
+                  onClick={(e) => handleDelete(e)}
+                  className="btn btn-outline-danger"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-
-            ))
-          }
+          ))}
         </tbody>
       </table>
-      </div>
-    )
-}
+    </div>
+  );
+};
 export default View;
