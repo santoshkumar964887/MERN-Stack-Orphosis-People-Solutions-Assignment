4d99ab115920676fbbf8d780d5cc1ddeb2999bab
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-13 05:43:17 +0530

    posting data into server

--- client-side/src/components/create/create.jsx
+++ client-side/src/components/create/create.jsx
@@ -1,34 +1,77 @@
 import React from 'react'
-
+import axios from '../axios/axios';
 export default class create extends React.Component {
     constructor(){
         super();
-        
+        this.state={
+          fname:"",
+          lname:"",
+          email:"",
+          bob: "",
+          bio:""
+        }
         
     }
+   Handlechange=(e)=>{
+     if(e.target.name==="fname"){
+      this.setState({fname:e.target.value})
+     }
+     else if(e.target.name==='lname'){
+      this.setState({lname:e.target.value})
+     }
+     else if(e.target.name==='email'){
+      this.setState({email:e.target.value})
+     }
+     else if(e.target.name==='bob'){
+      this.setState({bob:e.target.value})
+     }
+     else if(e.target.name==='bio'){
+      this.setState({bio:e.target.value})
+     }};
+     onclickHandler=(e)=>{
+       e.preventDefault();
+       axios.post('/',{
+        "firstName":this.state.fname,
+     "LastName":this.state.lname,
+     "Email": this.state.email,
+     "BOB":this.state.bob,
+     "Short_BIO":this.state.bio
+   }).then(res=>console.log(res))
+       this.setState({
+        fname:"",
+        lname:"",
+        email:"",
+        bob: "",
+        bio:""
+       })
+      alert("your data has been recoded")
+     }
+     
+
     render(){
     return (
-        <form>
-  <div className="form-group">
+        <form className="container">
+  <div className="form-group ">
     <label for="formGroupExampleInput">FirstName</label>
-    <input type="text" className="form-control" id="formGroupExampleInput" placeholder="FirstName"/>
+    <input type="text" className="form-control" name="fname" value={this.state.fname} onChange={this.Handlechange} id="formGroupExampleInput" placeholder="FirstName" required/>
   </div>
   <div className="form-group">
     <label for="formGroupExampleInput2">LastNamel</label>
-    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="LastNamel"/>
+    <input type="text" className="form-control" name="lname" value={this.state.lname} onChange={this.Handlechange} id="formGroupExampleInput2" placeholder="LastNamel" required/>
   </div>
     <div className="form-group">
     <label for="formGroupExampleInput2">Email</label>
-    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="email@example.com"/>
+    <input type="text" className="form-control" name="email" value={this.state.email} onChange={this.Handlechange}  id="formGroupExampleInput2" placeholder="email@example.com" required/>
   </div>
   <div className="form-group">
     <label for="formGroupExampleInput2">DOB</label>
-    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="BOB"/>
+    <input type="text" className="form-control" name="bob" value={this.state.bob} onChange={this.Handlechange} id="formGroupExampleInput2" placeholder="BOB" required/>
   </div>
   <div className="form-group">
     <label for="formGroupExampleInput2">Short BIO</label>
-    <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Short BIO"/>
+    <input type="text" className="form-control" name="bio" value={this.state.bio} onChange={this.Handlechange} id="formGroupExampleInput2" placeholder="Short BIO" required/>
   </div>
+  <button type="submit" onClick={this.onclickHandler} class="btn btn-primary">Primary</button>
 </form>
     );
 }
