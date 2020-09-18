10dce3cfcacf0857c443f8314d5754033246a67c
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-18 16:31:10 +0530

    changes package.json for depolying to production

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ app.use("/api/v1/user", tourRouter);
 app.all("*", (req, res, next) => {
   next( new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client-side/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client-side/build', 'index.html'));
+  });
+}
 app.use(globalErrorHandler.globalError);
 
 module.exports = app;
