f8f097948d4e7c4bd6fd67896767a9da71c48b7f
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-18 16:54:45 +0530

    deleting one lock file to slove heroku error

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const mongooes = require("mongoose");
+const bodyParser = require('body-parser');
 dotenv.config({ path: "./config.env" });
 //connecting with MongoDB server
+const port = process.env.PORT || 9000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongooes
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -13,8 +17,17 @@ mongooes
   .then((con) => {
     console.log("data base connected");
   });
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client-side/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client-side/build', 'index.html'));
+    });
+  }
 // Listening to  server on port number 9000
-const port = process.env.PORT || 9000;
+
 app.listen(port, () => {
   console.log("App is running on port number ", port);
 });
